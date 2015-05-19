d5a05f7433f8636fa18957b3dd2aeb7b3dc16390
cfortier2 <cfortier2@gmail.com>
2015-05-19 17:37:11 -0400

    restructure folder

--- v1/fleet_units/fleet_bootstrap.sh
+++ v1/fleet_units/fleet_bootstrap.sh
@@ -14,18 +14,12 @@
 #   > worker nodes:
 #     > mesos-slave
 
-# if .zookeeper exists, use its value
-if [[ -f /home/core/.zookeeper ]]
-then
-  echo "Reading zookeeper value from /home/core/.zookeeper"
-  ZOOKEEPER=$(cat /home/core/.zookeeper)
-else
-  # get the zookeeper address
-  echo -e "\nZookeeper Configuration:"
-  echo "Our default is to list the private DNS name of the ELB for our masters. Ex 'internal-coreos-control-etcd-852307729.us-west-2.elb.amazonaws.com:2181'"
-  echo "You can also supply a comma separated list of ip:port. Ex. '10.43.1.22:2181,10.43.3.128:2181,10.43.2.193:2181'"
-  read -p "Enter Zookeeper value: " ZOOKEEPER
-fi
+# get the zookeeper address
+echo -e "\nZookeeper Configuration:"
+echo "Our default is to list the private DNS name of the ELB for our masters. Ex 'internal-coreos-control-etcd-852307729.us-west-2.elb.amazonaws.com:2181'"
+echo "You can also supply a comma separated list of ip:port. Ex. '10.43.1.22:2181,10.43.3.128:2181,10.43.2.193:2181'"
+read -p "Enter Zookeeper value: " ZOOKEEPER
+
 # update the scripts with zookeeper value
 sed -i "s/<ZK_ELB>/$ZOOKEEPER/g" *.service
 

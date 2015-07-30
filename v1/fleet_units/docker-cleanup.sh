fb1a7e7ac62cea0006a77ae17b9e01aaf80707c1
cfortier2 <cfortier2@gmail.com>
2015-07-30 14:57:42 -0400

    docker-cleanup.sh tweak

--- v1/fleet_units/docker-cleanup.sh
+++ v1/fleet_units/docker-cleanup.sh
@@ -5,7 +5,7 @@
 if [[ $(/usr/bin/docker ps -a | grep Exited | awk '{print $1 "\\t" $7 "\\t" $10}' | grep week | wc -l) -ne 0 ]]
 then
   echo "Starting docker cleanup with a max of: $MAX_DOCKER"
-  /usr/bin/docker ps -a | grep Exited | awk '{print $1 "\\t" $7 "\\t" $10}' | grep week | awk \'{print $1}\' | head -n $MAX_DOCKER | xargs /usr/bin/docker rm -v
+  /usr/bin/docker ps -a | grep Exited | awk '{print $1 "\\t" $7 "\\t" $10}' | grep week | awk '{print $1}' | head -n $MAX_DOCKER | xargs /usr/bin/docker rm -v
   /usr/bin/docker images -a | grep none | awk '{print $3}' | xargs /usr/bin/docker rmi
   echo "cleanup complete"
 else

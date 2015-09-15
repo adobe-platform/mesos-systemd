529bc38df06c75710b793fb3245e3e2c2ac34305
chr0n1x <heilong24@gmail.com>
2015-09-15 16:24:35 -0400

    setup/containers: missing command

--- v2/setup/containers.sh
+++ v2/setup/containers.sh
@@ -4,7 +4,7 @@ source /etc/environment
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 
-behance/docker-gocron-logrotate
+docker pull behance/docker-gocron-logrotate
 docker pull behance/docker-sumologic
 docker pull behance/docker-dd-agent
 

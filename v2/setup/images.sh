78629838f7f5f71737933e2ebad7acea6dee9567
chr0n1x <heilong24@gmail.com>
2015-10-01 18:08:14 -0400

    v2/setup/images.sh: remove unused env vars

--- v2/setup/images.sh
+++ v2/setup/images.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-source /etc/environment
-export ETCDCTL_PEERS="http://$ETCDCTL_PEERS_ENDPOINT"
-
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 docker pull behance/docker-gocron-logrotate
 docker pull behance/docker-sumologic

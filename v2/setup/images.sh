a2c085b8e8395912c4c7386c529d6aa145b36d27
chr0n1x <heilong24@gmail.com>
2015-10-21 14:58:28 -0400

    setup/images: pull regular & syslog versions of sumologic image

--- v2/setup/images.sh
+++ v2/setup/images.sh
@@ -2,5 +2,6 @@
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 docker pull behance/docker-gocron-logrotate
-docker pull behance/docker-sumologic
+docker pull behance/docker-sumologic:latest
+docker pull behance/docker-sumologic:syslog-latest
 docker pull behance/docker-dd-agent

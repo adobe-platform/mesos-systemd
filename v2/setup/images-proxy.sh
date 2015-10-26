f5ec9745d0b048c4e84d6d2f6757fed8764aafd0
Israel Derdik <derdik@adobe.com>
2015-10-26 18:00:26 -0400

    added new proxy systemd unit

--- v2/setup/images-proxy.sh
+++ v2/setup/images-proxy.sh
@@ -10,8 +10,11 @@ export ETCDCTL_PEERS="http://$ETCDCTL_PEERS_ENDPOINT"
 
 etcdctl set /images/capcom       "behance/capcom:latest"
 etcdctl set /images/capcom2      "behance/capcom:latest"
+etcdctl set /images/proxy        "nginx:1.9.5"
+
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 docker pull behance/mesos-proxy-setup
 docker pull $(etcdctl get /images/capcom)
 docker pull $(etcdctl get /images/capcom2)
+docker pull $(etcdctl get /images/proxy)

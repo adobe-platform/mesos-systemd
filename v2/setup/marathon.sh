cecfd7e79f699a7f40e0a48f73b5838301781991
anshu pande <anshu@adobe.com>
2015-10-27 12:58:25 -0400

    removed a bad copy/paste

--- v2/setup/marathon.sh
+++ v2/setup/marathon.sh
@@ -8,7 +8,6 @@ sudo docker run --rm \
     -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
      us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .marathon
 
-SYSDIG_KEY=$(sudo cat ${HOMEDIR}/.marathon)
 
 etcdctl set /marathon_user $MARATHON_USER
 etcdctl set /marathon_password $MARATHON_PASSWORD

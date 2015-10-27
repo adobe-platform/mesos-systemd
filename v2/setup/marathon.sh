900ae99275217f3cb5cee557337faad49728135b
anshu pande <anshu@adobe.com>
2015-10-27 11:22:16 -0400

    passing marathon user and password as variables from secure S3 bucket

---
+++ v2/setup/marathon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /etc/environment
+
+HOMEDIR=$(eval echo "~`whoami`")
+
+sudo docker run --rm \
+    -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+     us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .marathon
+
+SYSDIG_KEY=$(sudo cat ${HOMEDIR}/.marathon)
+
+etcdctl set /marathon_user $marathon_user
+etcdctl set /marathon_password $marathon_password

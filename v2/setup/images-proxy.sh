a127d516ba06c8f98f23724957e9c1a547c2c414
chr0n1x <heilong24@gmail.com>
2015-11-19 15:46:42 -0500

    v2/setup/images*: lock image setting

--- v2/setup/images-proxy.sh
+++ v2/setup/images-proxy.sh
@@ -8,10 +8,15 @@ fi
 
 export ETCDCTL_PEERS="http://$ETCDCTL_PEERS_ENDPOINT"
 
+if [ "$(etcdctl get images-proxy-bootstrapped)" == "true" ]; then
+    echo "proxy-tier images already bootstrapped, skipping"
+    exit 0
+fi
+etcdctl set images-proxy-bootstrapped true
+
 etcdctl set /images/capcom       "behance/capcom:latest"
 etcdctl set /images/capcom2      "behance/capcom:latest"
 etcdctl set /images/proxy        "nginx:1.9.5"
-
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 docker pull behance/mesos-proxy-setup
--- v2/setup/images-worker.sh
+++ v2/setup/images-worker.sh
@@ -8,6 +8,12 @@ fi
 
 export ETCDCTL_PEERS="http://$ETCDCTL_PEERS_ENDPOINT"
 
+if [ "$(etcdctl get images-worker-bootstrapped)" == "true" ]; then
+    echo "worker-tier images already bootstrapped, skipping"
+    exit 0
+fi
+etcdctl set images-worker-bootstrapped true
+
 etcdctl set /images/mesos-slave  "mesosphere/mesos-slave:0.22.1-1.0.ubuntu1404"
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x

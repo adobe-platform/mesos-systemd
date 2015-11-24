d61b912bdc0c5019ecbd0dcc9c4214e9653a2c1c
Nina Berg <nikb100@gmail.com>
2015-11-24 15:48:46 -0500

    etcd2 unit files and scripts

---
+++ v2/util/etcd-peers-dropin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# NOTE: this needs to be run with sudo privileges
+
+mkdir -p /etc/systemd/system/etcd2.service.d
+DROPIN_FILE=/etc/systemd/system/etcd2.service.d/30-etcd_peers.conf
+
+cat > $DROPIN_FILE <<EOF
+[Service]
+# Load the other hosts in the etcd leader autoscaling group from file
+EnvironmentFile=/etc/sysconfig/etcd-peers
+EOF
+
+chown root:root $DROPIN_FILE
+chmod 0644 $DROPIN_FILE

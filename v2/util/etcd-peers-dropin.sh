18cc1dd29ca46db0e4675a8740e42d880db301bc
Nina Berg <nikb100@gmail.com>
2015-11-30 11:17:36 -0500

    Move etcd-peers stuff out of init

--- v2/util/etcd-peers-dropin.sh
+++ v2/util/etcd-peers-dropin.sh
@@ -2,6 +2,8 @@
 
 # NOTE: this needs to be run with sudo privileges
 
+echo "-------Control node found, setting up etcd peers-------"
+
 mkdir -p /etc/systemd/system/etcd2.service.d
 DROPIN_FILE=/etc/systemd/system/etcd2.service.d/30-etcd_peers.conf
 
@@ -13,3 +15,6 @@ EOF
 
 chown root:root $DROPIN_FILE
 chmod 0644 $DROPIN_FILE
+
+cp "${SCRIPTDIR}/v2/util-units/etcd-peers.service" /etc/systemd/system/
+systemctl start etcd-peers

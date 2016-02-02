6c238bf5346d9107f57f91c5cc545add5a926abe
Israel Derdik <derdik@adobe.com>
2016-02-02 10:49:33 -0500

    marathon 15

--- v2/setup/images-control.sh
+++ v2/setup/images-control.sh
@@ -15,7 +15,7 @@ etcdctl set images-control-bootstrapped true
 etcdctl set /images/chronos      "mesosphere/chronos:chronos-2.4.0-0.1.20151007110204.ubuntu1404-mesos-0.26.0-0.2.145.ubuntu1404"
 etcdctl set /images/fd           "behance/flight-director:latest"
 etcdctl set /images/hud          "behance/flight-director-hud:latest"
-etcdctl set /images/marathon     "mesosphere/marathon:v0.13.0"
+etcdctl set /images/marathon     "mesosphere/marathon:v0.15.0"
 etcdctl set /images/mesos-master "mesosphere/mesos-master:0.26.0-0.2.145.ubuntu1404"
 etcdctl set /images/zk-exhibitor "behance/docker-zk-exhibitor:latest"
 etcdctl set /images/cfn-signal   "behance/docker-cfn-bootstrap:latest"

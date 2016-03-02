da51822105414dca2605871657e902f60e4a87aa
Israel Derdik <derdik@adobe.com>
2016-03-02 17:52:23 -0500

    hybrid units

--- v2/util/setup-credentials.sh
+++ v2/util/setup-credentials.sh
@@ -9,8 +9,13 @@ source /etc/environment
 if [ ! -z $SECURE_FILES ]; then
     # have to use  us-east-1 - aws tool does not recognize anything else
     # for S3 download
+    AWS_CREDS=""
+    if [ ! -z $AWS_ACCESS_KEY ]; then
+        AWS_CREDS=" -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+         -e AWS_SECRET_KEY=$AWS_SECRET_KEY  "
+    fi
     sudo docker run --rm \
-        -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+        -v ${HOMEDIR}:/data/  $AWS_CREDS behance/docker-aws-s3-downloader \
          us-east-1 $CONTROL_TIER_S3SECURE_BUCKET $SECURE_FILES
 
     # must chown all files to core for use

36b9575a04aade184dec1db5db81a88904e1cce5
Israel Derdik <derdik@adobe.com>
2016-03-03 11:00:44 -0500

    fleet units for hybrid + creds downlaod

--- v2/setup/sumologic.sh
+++ v2/setup/sumologic.sh
@@ -4,8 +4,14 @@ source /etc/environment
 
 HOMEDIR=$(eval echo "~`whoami`")
 
+AWS_CREDS=""
+if [ ! -z $AWS_ACCESS_KEY ]; then
+    AWS_CREDS=" -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY \
+     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY "
+fi
+
 sudo docker run --rm \
-    -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+    -v ${HOMEDIR}:/data/ $AWS_CREDS behance/docker-aws-s3-downloader \
      us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .sumologic
 
 SUMOLOGIC_ACCESS_ID=$(sudo cat ${HOMEDIR}/.sumologic | grep ID | cut -d= -f2)

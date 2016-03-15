72e1f8ca051245157bde639842b17d8ac78bce39
anshu pande <anshu@adobe.com>
2016-03-15 11:43:25 -0400

    testing framework authentication between master and slave

---
+++ v2/setup/mesos-slave.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /etc/environment
+
+HOMEDIR=$(eval echo "~`whoami`")
+
+AWS_CREDS=""
+if [ ! -z $AWS_ACCESS_KEY ]; then
+    AWS_CREDS=" -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY \
+     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY "
+fi
+
+sudo docker run --rm \
+    -v ${HOMEDIR}:/data/ $AWS_CREDS behance/docker-aws-s3-downloader \
+     us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .mesos-slave

ac7fa127e1305e791ed4601a321c402d76970cc7
anshu pande <anshu@adobe.com>
2016-03-16 12:55:26 -0400

    modified logic

--- v2/util/credential-master.sh
+++ v2/util/credential-master.sh
@@ -2,12 +2,14 @@
 
 source /etc/environment
 
+HOMEDIR=$(eval echo "~`whoami`")
+
 principal=$(etcdctl get principal)
 secret=$(etcdctl get secret)
 
-sudo mkdir /home/core/mesos-master
-sudo touch /home/core/mesos-master/passwd
+mkdir $HOMEDIR/mesos-master
+touch $HOMEDIR/mesos-master/passwd
 
 
 
-echo "'$(eval echo $principal)' '$(eval echo $secret)'" > /home/core/mesos-master/passwd
+echo "$(eval echo $principal) $(eval echo $secret)" > $HOMEDIR/mesos-master/passwd
--- v2/util/credential-slave.sh
+++ v2/util/credential-slave.sh
@@ -2,11 +2,13 @@
 
 source /etc/environment
 
+HOMEDIR=$(eval echo "~`whoami`")
+
 principal=$(etcdctl get principal)
 secret=$(etcdctl get secret)
 
 
-sudo mkdir /home/core/mesos-slave
-sudo touch /home/core/mesos-slave/passwd
+mkdir $HOMEDIR/mesos-slave
+touch $HOMEDIR/mesos-slave/passwd
 
-echo "'$(eval echo $principal)' '$(eval echo $secret)'" > /home/core/mesos-slave/passwd
+echo "$(eval echo $principal) $(eval echo $secret)" > $HOMEDIR/mesos-slave/passwd

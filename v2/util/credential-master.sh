ea0b65ebef991f215c5c4899fc8b05a1052ffb41
anshu pande <anshu@adobe.com>
2016-03-16 12:33:19 -0400

    building logic to pass principal to file

--- v2/util/credential-master.sh
+++ v2/util/credential-master.sh
@@ -2,11 +2,12 @@
 
 source /etc/environment
 
-Environment="principal=etcdctl get principal"
-Environment="secret=etcdctl get secret"
+principal=$(etcdctl get principal)
+secret=$(etcdctl get secret)
 
 sudo mkdir /home/core/mesos-master
 sudo touch /home/core/mesos-master/passwd
 
 
-echo "$($principal) $($secret)" >> /home/core/mesos-master/passwd
+
+echo "'$(eval echo $principal)' '$(eval echo $secret)'" > /home/core/mesos-master/passwd
--- v2/util/credential-slave.sh
+++ v2/util/credential-slave.sh
@@ -2,11 +2,11 @@
 
 source /etc/environment
 
-Environment="principal=etcdctl get principal"
-Environment="secret=etcdctl get secret"
+principal=$(etcdctl get principal)
+secret=$(etcdctl get secret)
 
 
 sudo mkdir /home/core/mesos-slave
 sudo touch /home/core/mesos-slave/passwd
 
-echo "$($principal) $($secret)" >> /home/core/mesos-slave/passwd
+echo "'$(eval echo $principal)' '$(eval echo $secret)'" > /home/core/mesos-slave/passwd

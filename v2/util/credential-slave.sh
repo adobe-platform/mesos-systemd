2bb7afd3a7168a43bb5c4f58437091f2d73c6140
anshu pande <anshu@adobe.com>
2016-03-18 16:40:05 -0400

    testing

--- v2/util/credential-slave.sh
+++ v2/util/credential-slave.sh
@@ -7,8 +7,10 @@ HOMEDIR=$(eval echo "~`whoami`")
 principal=$(etcdctl get principal)
 secret=$(etcdctl get secret)
 
-
 mkdir $HOMEDIR/mesos-slave
 touch $HOMEDIR/mesos-slave/passwd
 
+
+
 echo "$(eval echo $principal) $(eval echo $secret)" > $HOMEDIR/mesos-slave/passwd
+

c6484096dc968eb30d5b685232cdd0e06a60cbb4
anshu pande <anshu@adobe.com>
2016-03-31 16:06:18 -0400

    testing by enabling authenticate option

--- v2/util/credential-master.sh
+++ v2/util/credential-master.sh
@@ -6,6 +6,8 @@ HOMEDIR=$(eval echo "~`whoami`")
 
 principal=$(etcdctl get principal)
 secret=$(etcdctl get secret)
+principal1=$(etcdctl get principal1)
+secret1=$(etcdctl get secret1)
 
 mkdir $HOMEDIR/mesos-master
 touch $HOMEDIR/mesos-master/passwd
@@ -13,3 +15,4 @@ touch $HOMEDIR/mesos-master/passwd
 
 
 echo "$(eval echo $principal) $(eval echo $secret)" > $HOMEDIR/mesos-master/passwd
+echo "$(eval echo $principal1) $(eval echo $secret1)" >> $HOMEDIR/mesos-master/passwd

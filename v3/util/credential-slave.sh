12aec491fb4e7f8c002bcb801596108d605b3593
anshu pande <anshu@adobe.com>
2016-04-18 16:08:27 -0400

    fix bug from issue 8182

--- v3/util/credential-slave.sh
+++ v3/util/credential-slave.sh
@@ -9,7 +9,9 @@ secretslave=$(etcdctl get /frameworkauth/secret)
 
 mkdir $HOMEDIR/mesos-slave
 touch $HOMEDIR/mesos-slave/passwd
-
+sudo cp /root/.dockercfg /home/core/.dockercfg
+sudo chown core:core /home/core/.dockercfg
+chmod 0600 /home/core/mesos-slave/passwd
 
 
 echo "$(eval echo $principalslave) $(eval echo $secretslave)" > $HOMEDIR/mesos-slave/passwd

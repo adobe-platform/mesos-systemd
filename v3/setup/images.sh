110eb268abeb75348b1b374d1eda0d381ee9092c
tseng <tseng@adobe.com>
2016-04-19 16:14:34 -0700

    rever images.sh

--- v3/setup/images.sh
+++ v3/setup/images.sh
@@ -5,9 +5,6 @@ if [ "$(etcdctl get images-base-bootstrapped)" == "true" ]; then
     exit 0
 fi
 etcdctl set images-base-bootstrapped true
-etcdctl mkdir splunk
-etcdctl set /splunk/SPLUNK_FORWARD_SERVER "ds.adobesecaas.com:8089"
-etcdctl set /images/splunkforwarder "adobeplatform/docker-splunk:latest"
 
 # pull down images serially to avoid a FS layer clobbering bug in docker 1.6.x
 docker pull behance/docker-gocron-logrotate

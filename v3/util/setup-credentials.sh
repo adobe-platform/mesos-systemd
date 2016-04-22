f54b47349793c9fe02f6a3fd211f18330a2ac68e
chr0n1x <heilong24@gmail.com>
2016-04-22 17:20:42 -0400

    v3: split out etcd seed logic from credentials

--- v3/util/setup-credentials.sh
+++ v3/util/setup-credentials.sh
@@ -9,39 +9,6 @@ source /etc/environment
 TABLE=`sudo echo $SECRETS_TABLE`
 docker pull behance/docker-aws-secrets-downloader:latest
 
-# Get all available secrets and configs
-AV_SECRETS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets`
-AV_CONFIGS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key configs`
-
-printf "#!/bin/bash\nsource /etc/environment\n" >  ${HOMEDIR}/.etcd_loader.sh
-
-echo "$AV_SECRETS" | while read line ; do
-    SECRET=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name $line`
-    SECRET_TYPE=`echo $SECRET | cut -d' ' -f2`
-    SECRET_PATH=`echo $SECRET | cut -d' ' -f3`
-    SECRET_VAL=`echo $SECRET | cut -d' ' -f4-`
-
-    if [[ "$SECRET_TYPE" = "etcd" ]]; then
-        echo "Setting ETCD value for secret: $SECRET_PATH"
-        #etcdctl set $SECRET_PATH $SECRET_VAL &>/dev/null
-        printf "etcdctl set -- %s \"%s\" &>/dev/null \n" "$SECRET_PATH" "$SECRET_VAL" >> ${HOMEDIR}/.etcd_loader.sh
-    fi
-done
-
-echo "$AV_CONFIGS" | while read line ; do
-    CONFIG=`docker run behance/docker-aws-secrets-downloader --table $TABLE --key configs --name $line`
-    CONFIG_PATH=`echo $CONFIG | cut -d' ' -f1`
-    CONFIG_VAL=`echo $CONFIG | cut -d' ' -f2-`
-
-    echo "Setting ETCD value for config: $CONFIG_PATH"
-    #etcdctl set $CONFIG_PATH $CONFIG_VAL &>/dev/null
-    printf "etcdctl set %s \"%s\" &>/dev/null \n" "$CONFIG_PATH" "$CONFIG_VAL" >> ${HOMEDIR}/.etcd_loader.sh
-done
-
-chmod +x ${HOMEDIR}/.etcd_loader.sh
-${HOMEDIR}/.etcd_loader.sh
-rm ${HOMEDIR}/.etcd_loader.sh
-
 # Create a dockercfg
 DOCKERCFG_CONTENTS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name DOCKERCFG --format plain`
 echo "$DOCKERCFG_CONTENTS" > /home/${OWNER}/.dockercfg
@@ -51,13 +18,13 @@ sudo cp /home/${OWNER}/.dockercfg /root/.dockercfg
 # Save the Git Pull Key (must decode base64 since using "plain" option)
 GIT_PULL_KEY_CONTENTS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name GIT_PULL_KEY --format plain | base64 -d`
 echo "$GIT_PULL_KEY_CONTENTS" > ${HOMEDIR}/.ssh/id_rsa
-
 # Check to see if the key has a "part 2"
+#     Get all available secrets and configs
+AV_SECRETS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets`
 if [[ $AV_SECRETS == *"GIT_PULL_KEY_PART_2"* ]]; then
     GIT_PULL_KEY_CONTENTS_PART_TWO=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name GIT_PULL_KEY_PART_2 --format plain | base64 -d`
     echo "$GIT_PULL_KEY_CONTENTS_PART_TWO" >> ${HOMEDIR}/.ssh/id_rsa
 fi
-
 sudo chown -R ${OWNER}:${OWNER} ${HOMEDIR}/.ssh/id_rsa
 chmod 600 ${HOMEDIR}/.ssh/id_rsa
 

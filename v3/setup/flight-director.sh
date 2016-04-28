4649be941d4c9bed35c6a29264073e8a021f0e67
chr0n1x <heilong24@gmail.com>
2016-04-28 15:13:39 -0400

    RDSPASSWORD: download, seed into etcd, update FD units

--- v3/setup/flight-director.sh
+++ v3/setup/flight-director.sh
@@ -5,18 +5,16 @@ source /etc/environment
 if [ "${NODE_ROLE}" != "control" ]  && [ "${NODE_ROLE}" != "it-hybrid" ]; then
     exit 0
 fi
-
 if [ "$(etcdctl get fd-bootstrapped)" == "true" ]; then
     exit 0
 fi
-etcdctl set fd-bootstrapped true
 
+etcdctl set fd-bootstrapped true
 etcdctl setdir FD
 etcdctl set /FD/FD_API_SERVER_PORT 2001
 etcdctl set /FD/FD_CHRONOS_MASTER "$FLIGHT_DIRECTOR_CHRONOS_ENDPOINT"
 etcdctl set /FD/FD_DB_DATABASE "$FLIGHT_DIRECTOR_DB_NAME"
 etcdctl set /FD/FD_DB_ENGINE mysql
-etcdctl set /FD/FD_DB_PASSWORD "$FLIGHT_DIRECTOR_DB_PASSWORD"
 etcdctl set /FD/FD_DB_PATH "$FLIGHT_DIRECTOR_DB_PATH"
 etcdctl set /FD/FD_DB_USERNAME "$FLIGHT_DIRECTOR_DB_USERNAME"
 etcdctl set /FD/FD_DOCKERCFG_LOCATION file:///root/.dockercfg
@@ -33,5 +31,4 @@ etcdctl set /FD/FD_MESOS_MASTER "$FLIGHT_DIRECTOR_MESOS_ENDPOINT"
 etcdctl set /FD/FD_MARATHON_MASTER_PROTOCOL http
 etcdctl set /FD/FD_ALLOW_MARATHON_UNVERIFIED_TLS false
 etcdctl set /FD/FD_MESOS_MASTER_PROTOCOL http
-
 etcdctl set /FD/AUTHORIZER_TYPE airlock

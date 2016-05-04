7e9d76537a33011d1a9884bdb69326bc0d7ab65e
Matt Fuller <fuller@adobe.com>
2016-05-04 13:28:42 -0400

    removing git key setup

--- v3/util/setup-credentials.sh
+++ v3/util/setup-credentials.sh
@@ -15,19 +15,6 @@ echo "$DOCKERCFG_CONTENTS" > /home/${OWNER}/.dockercfg
 sudo chown -R ${OWNER}:${OWNER} /home/${OWNER}/.dockercfg
 sudo cp /home/${OWNER}/.dockercfg /root/.dockercfg
 
-# Save the Git Pull Key (must decode base64 since using "plain" option)
-GIT_PULL_KEY_CONTENTS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name GIT_PULL_KEY --format plain | base64 -d`
-echo "$GIT_PULL_KEY_CONTENTS" > ${HOMEDIR}/.ssh/id_rsa
-# Check to see if the key has a "part 2"
-#     Get all available secrets and configs
-AV_SECRETS=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets`
-if [[ $AV_SECRETS == *"GIT_PULL_KEY_PART_2"* ]]; then
-    GIT_PULL_KEY_CONTENTS_PART_TWO=`sudo docker run behance/docker-aws-secrets-downloader --table $TABLE --key secrets --name GIT_PULL_KEY_PART_2 --format plain | base64 -d`
-    echo "$GIT_PULL_KEY_CONTENTS_PART_TWO" >> ${HOMEDIR}/.ssh/id_rsa
-fi
-sudo chown -R ${OWNER}:${OWNER} ${HOMEDIR}/.ssh/id_rsa
-chmod 600 ${HOMEDIR}/.ssh/id_rsa
-
 # ensure that we have a public key for our RSA key and that it's authorized
 if [ -f ${HOMEDIR}/.ssh/id_rsa ]; then
     ssh-keygen -f ${HOMEDIR}/.ssh/id_rsa -y > ${HOMEDIR}/.ssh/id_rsa.pub

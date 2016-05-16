a097ff1048e520035a8767b0d505534713321bc5
anshu pande <anshu@adobe.com>
2016-05-16 11:56:39 -0400

    adding kevin's comments

--- v3/setup/etcdctl-auth.sh
+++ v3/setup/etcdctl-auth.sh
@@ -2,10 +2,6 @@
 
 source /etc/environment
 
-#Disable authentication
-
-#curl  -L http://127.0.0.1:2379/v2/auth/enable -XDELETE
-
 etcdctl set /etcdctl/config/root-user root
 etcdctl set /etcdctl/config/read-user read-user
 etcdctl set /etcdctl/config/write-user write-user
@@ -22,60 +18,36 @@ if [[ ! -d $CRED_DIR ]]; then
     sudo mkdir $CRED_DIR -p
 fi
 
-#add a root user
+# add root-user
+sudo echo '{"user":"'${ROOT_USENAME}'", "password":"'${ROOT_PASSWORD}'"}' > $CRED_DIR/root-user.json
+curl  -L http://127.0.0.1:2379/v2/auth/users/$ROOT_USERNAME -XPUT -d "@$CRED_DIR/root-user.json"
 
-sudo echo '{"user":"'$(eval echo $ROOT_USERNAME)'", "password":"'$(eval echo $ROOT_PASSWORD)'"}' > $CRED_DIR/root-user.json
-curl  -L http://127.0.0.1:2379/v2/auth/users/$(eval echo $ROOT_USERNAME) -XPUT -d "@$CRED_DIR/root-user.json"
+# add read-user
+sudo echo '{"user":"'${READ_USERNAME}'", "password":"'${READ_PASSWORD}'"}' > $CRED_DIR/read-user.json
+curl   -L http://127.0.0.1:2379/v2/auth/users/$READ_USERNAME -XPUT -d "@$CRED_DIR/read-user.json"
 
+# add write-user
+sudo echo '{"user":"'${WRITE_USERNAME}'", "password":"'${WRITE_PASSWORD}'"}' > $CRED_DIR/write-user.json
+curl  -L http://127.0.0.1:2379/v2/auth/users/$WRITE_USERNAME -XPUT -d "@$CRED_DIR/write-user.json"
 
-#add a non root read only user
-sudo echo '{"user":"'$(eval echo $READ_USERNAME)'", "password":"'$(eval echo $READ_PASSWORD)'"}' > $CRED_DIR/read-user.json
-curl   -L http://127.0.0.1:2379/v2/auth/users/$(eval echo $READ_USERNAME) -XPUT -d "@$CRED_DIR/read-user.json"
+# read access
+etcdctl role add read-only
+etcdctl role grant read-only -path '/*' -read
 
-#add a non root readwrite user
-sudo echo '{"user":"'$(eval echo $WRITE_USERNAME)'", "password":"'$(eval echo $WRITE_PASSWORD)'"}' > $CRED_DIR/write-user.json
-curl  -L http://127.0.0.1:2379/v2/auth/users/$(eval echo $WRITE_USERNAME) -XPUT -d "@$CRED_DIR/write-user.json"
+# write access
+etcdctl role add read-write
+etcdctl role grant read-write -path '/*' -readwrite
 
-#Create a read only role:
+# Give read-user read access
+sudo echo '{"user": "'${READ_USERNAME}'", "grant": ["read-only"]}' > $CRED_DIR/read-only.json
+curl  -L http://127.0.0.1:2379/v2/auth/users/$READ_USERNAME -XPUT -d "@$CRED_DIR/read-only.json"
 
-etcdctl role add readonlyrole
+# Give read-write write access
+sudo echo '{"user": "'${WRITE_USERNAME}'", "grant": ["read-write"]}' > $CRED_DIR/read-write.json
+curl  -L http://127.0.0.1:2379/v2/auth/users/$WRITE_USERNAME -XPUT -d "@$CRED_DIR/read-write.json"
 
-#Give read only access to this role:
-
-etcdctl role grant readonlyrole -path '/*' -read
-
-#Create a readwrite only role:
-
-etcdctl role add readwriterole
-
-#Give readwrite only access to this role:
-
-etcdctl role grant readwriterole -path '/*' -readwrite
-
-#Give read only non root user read only role
-
-sudo echo '{"user": "'$(eval echo $READ_USERNAME)'", "grant": ["readonlyrole"]}' > $CRED_DIR/readonlyrole.json
-
-curl  -L http://127.0.0.1:2379/v2/auth/users/$(eval echo $READ_USERNAME) -XPUT -d "@$CRED_DIR/readonlyrole.json"
-
-#Give readwrite non root user readwrite role
-
-sudo echo '{"user": "'$(eval echo $WRITE_USERNAME)'", "grant": ["readwriterole"]}' > $CRED_DIR/readwriterole.json
-
-curl  -L http://127.0.0.1:2379/v2/auth/users/$(eval echo $WRITE_USERNAME) -XPUT -d "@$CRED_DIR/readwriterole.json"
-
-#revoke guest role read:write access
-
-#etcdctl role revoke guest -path '/*' -readwrite
-
-#Enable authentication
-
-curl  -L http://127.0.0.1:2379/v2/auth/enable -XPUT
-
-#etcdctl auth enable
-
-#Get authentication Status
-#curl -u $(echo "$(cat root.json |jq '.user')" | sed -e 's/^"//'  -e 's/"$//'):$(echo "$(cat root.json |jq '.password')" | sed -e 's/^"//'  -e 's/"$//') -L http://127.0.0.1:2379/v2/auth/users -XGET
+# Enable authentication
+curl -L http://127.0.0.1:2379/v2/auth/enable -XPUT
 
 sudo chmod 0600 $CRED_DIR/*
 sudo chown -R $(whoami):$(whoami) $CRED_DIR

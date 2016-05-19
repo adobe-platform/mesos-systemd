d2692c8c5697b63df3b4eea8edf59eaa251623b5
tseng <tseng@adobe.com>
2016-05-19 10:23:23 -0700

    changes suggested

--- v3/setup/splunk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# SPLUNK_FORWARD_SERVER is declard in infrastructure config/secrets.json
-
-source /etc/environment
-
-etcdctl set /images/splunkforwarder "adobeplatform/docker-splunk:latest"
